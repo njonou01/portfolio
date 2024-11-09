993eb1cf6a39de6d40c1907785b1c7a7a8a4b019
njonou01 <njonougaby01@gmail.com>
2024-11-09 09:31:46 +0100

    ScrollProgress amélioration

--- src/app/components/ScrollProgress/ScrollProgress.tsx
+++ src/app/components/ScrollProgress/ScrollProgress.tsx
@@ -7,16 +7,18 @@ const ScrollProgress: React.FC = () => {
     const handleScroll = () => {
       const totalScroll =
         document.documentElement.scrollHeight - window.innerHeight;
-      const currentProgress = (window.screenY / totalScroll) * 100;
+      const currentProgress = (window.scrollY / totalScroll) * 100;
       setScrollProgress(currentProgress);
     };
+
+    handleScroll();
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className="fixed top-0 left-0 w-full h-1 z-50">
+    <div className="fixed top-0 left-0 w-full h-1 z-[100]">
       <div
         className="h-full bg-gradient-to-r from-primary-500 to-primary-700"
         style={{ width: `${scrollProgress}%` }}
